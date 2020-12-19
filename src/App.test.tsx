a54e7a76219666cc84d6873c93579ad61db33775
ZainRamzan <64620737+ZainRamzanGorsi@users.noreply.github.com>
2020-12-19 21:10:38 +0500

    Update

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+
+
+test('renders main heading', () => {
+  const renderObj = render(<App />);
+  const linkElement = renderObj.getByText(/Timer App/i);
+
   expect(linkElement).toBeInTheDocument();
 });
+
